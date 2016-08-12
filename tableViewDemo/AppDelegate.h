38aa139431dce8acd8f4bf2287f0a79962625393
easyhaipi <easy_qingsong@163.com>
2016-08-12 10:00:01 +0800

    Initial Commit

---
+++ tableViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableViewDemo
+//
+//  Created by taobaichi on 16/8/12.
+//  Copyright © 2016年 taobaichi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
